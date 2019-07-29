079dc0445523f7697129678ea21517de5e0e78f1
Thomas61170 <Thomas61170@GB-LD1LPW10G044.mastek.com>
2019-07-29 09:59:33 +0100

    Company Entity Created

---
+++ jobapp/src/main/java/com/mastek/jobapp/entities/Company.java
@@ -0,0 +1,17 @@
+package com.mastek.jobapp.entities;
+
+public class Company {
+	
+	private int company_id;
+	
+	private String company_name;
+	
+	private String location;
+	
+	private String industry;
+	
+	public Company() {
+		System.out.println("Company Created");
+	}
+
+}
