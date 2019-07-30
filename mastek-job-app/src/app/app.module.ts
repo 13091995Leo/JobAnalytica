69a8ded524cd10a679fe849c01630c609b19f189
purpl3jam <Thomas61170@Mastek.Com>
2019-07-30 14:38:30 +0100

    Tom's stuff

--- mastek-job-app/src/app/app.module.ts
+++ mastek-job-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +24,9 @@ import { JobsearchComponent } from './jobsearch/jobsearch.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
