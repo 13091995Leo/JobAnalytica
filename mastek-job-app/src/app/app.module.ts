7e00ae225aab401822e06932ba18748ed989fa68
purpl3jam <Thomas61170@Mastek.Com>
2019-07-30 11:24:51 +0100

    Added all page components

--- mastek-job-app/src/app/app.module.ts
+++ mastek-job-app/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JobComponent } from './job/job.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UserComponent } from './user/user.component';
+import { CompanyloginComponent } from './companylogin/companylogin.component';
+import { CompanyComponent } from './company/company.component';
+import { JobsearchComponent } from './jobsearch/jobsearch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JobComponent
+    JobComponent,
+    UserloginComponent,
+    UserComponent,
+    CompanyloginComponent,
+    CompanyComponent,
+    JobsearchComponent
   ],
   imports: [
     BrowserModule,
