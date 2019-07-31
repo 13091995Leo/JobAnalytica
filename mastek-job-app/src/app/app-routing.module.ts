ea81ec2c13568024e187d7dcd8898f5c616c98d2
josh3103 <50618346+josh3103@users.noreply.github.com>
2019-07-31 11:48:00 +0100

    app routing/links

--- mastek-job-app/src/app/app-routing.module.ts
+++ mastek-job-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JobComponent } from './job/job.component';
 import { JobsearchComponent } from './jobsearch/jobsearch.component';
+import { CompanyComponent } from './company/company.component';
 
 
 const routes: Routes = [
   { path: 'job', component: JobComponent },
-  { path: 'jobsearch', component: JobsearchComponent }
+  { path: 'jobsearch', component: JobsearchComponent },
+  { path: 'companyPage', component: CompanyComponent}
 ];
 
 @NgModule({
