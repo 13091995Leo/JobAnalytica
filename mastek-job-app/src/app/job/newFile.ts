316166da1594d1f2c47695860211c070331af082
Mohammed Miah <Mohamme61167@Mastek.Com>
2019-07-31 10:08:57 +0100

    change again

---
+++ mastek-job-app/src/app/job/newFile.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { Company } from '../company';
+
+@Component({
+  selector: 'app-job',
+  templateUrl: './job.component.html',
+  styleUrls: ['./job.component.css']
+})
+export class JobComponent implements OnInit {
+
+
+
+  jobId: number
+  jobTitle: string
+  requirements: string
+  salary: number
+  location: string
+  currentCompany: Company
+  assignments: User[]
+  isEditable:boolean
+  isProjectFormVisible: boolean
+
+  isProjectFormValid: boolean
+  invalidFormMessage:String
+
+  allCompanys:Company[]
+  selectedCompanyId:number
+
+  allUsers:User[]
+  selectedUserId:number
+  constructor(private jobSvc:JobService) {
+    this.jobId = 1
+    this.jobTitle="Front End Developer"
+    this.requirements="Java, HTML, MySQL"
+    this.salary=35000
+
+    this.currentCompany ={
+      companyId: 12,
+      companyName: "Pepsicocacola",
+      location: "Afghanistan"
+    }
+
+    this.assignments = [
+      {userId:22, userName:"Triple H", locationPreference:"Cuba", speciality:"AngularJS"},  
+      {userId:23, userName:"Kim Jong Un", locationPreference:"North Korea", speciality:"Lambda"}
+    ]
+
+  }
+  }
+
+ngOnInit();{
+}
